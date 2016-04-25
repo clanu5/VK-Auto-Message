065418d69e5a27b125f97d7195dd24fd31074fe6
Alex Zhdanov <qwert2603@mail.ru>
2016-04-25 19:57:24 +0700

    1.1.2-beta release
    hide fab on scroll

---
+++ app/src/main/java/com/qwert2603/vkautomessage/record_list/FabHideBehavior.java
@@ -0,0 +1,31 @@
+package com.qwert2603.vkautomessage.record_list;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FabHideBehavior extends FloatingActionButton.Behavior {
+
+    public FabHideBehavior(Context context, AttributeSet attrs) {
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
+        if (dyConsumed > 0) {
+            CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+            child.animate().translationY(child.getHeight() + layoutParams.bottomMargin).start();
+        } else if (dyConsumed < 0) {
+            child.animate().translationY(0).start();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
